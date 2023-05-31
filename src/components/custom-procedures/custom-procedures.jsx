d6ae45be76883abc7ec9e1282a9d4bffe33185f5
CST1229 <68464103+CST1229@users.noreply.github.com>
2023-05-31 16:46:54 +0200

    the custom procedures dialog update

--- src/components/custom-procedures/custom-procedures.jsx
+++ src/components/custom-procedures/custom-procedures.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/jsx-no-bind */
 import PropTypes from 'prop-types';
 import React from 'react';
 import Modal from '../../containers/modal.jsx';
 import Box from '../box/box.jsx';
-import { defineMessages, injectIntl, intlShape, FormattedMessage } from 'react-intl';
+import {defineMessages, injectIntl, intlShape, FormattedMessage} from 'react-intl';
 
 import booleanInputIcon from './icon--boolean-input.svg';
 import textInputIcon from './icon--text-input.svg';
@@ -18,142 +19,129 @@ const messages = defineMessages({
     }
 });
 
-// for some reason the arrow is just gone?
-// so we add our own :D
-const dropdownCaret = '►';
-
 const BlockColorSection = props => (
-    <details>
-        <summary>{dropdownCaret} Block Color</summary>
-        <div style={{ marginLeft: '10px' }}>
-            <label>
-                <input
-                    value={props.blockColor}
-                    type="color"
-                    onChange={props.onBlockColorChange}
-                    style={{ marginRight: "6px" }}
-                />
-                Custom Color
-            </label>
-            <br></br>
-            <p><b>Presets</b></p>
-            <div>
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#4C97FF" }}
-                    onClick={() => props.setHexBlockColor("#4C97FF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#9966FF" }}
-                    onClick={() => props.setHexBlockColor("#9966FF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#CF63CF" }}
-                    onClick={() => props.setHexBlockColor("#CF63CF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FFBF00" }}
-                    onClick={() => props.setHexBlockColor("#FFBF00")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FFAB19" }}
-                    onClick={() => props.setHexBlockColor("#FFAB19")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#5CB1D6" }}
-                    onClick={() => props.setHexBlockColor("#5CB1D6")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#59C059" }}
-                    onClick={() => props.setHexBlockColor("#59C059")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FF8C1A" }}
-                    onClick={() => props.setHexBlockColor("#FF8C1A")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FF661A" }}
-                    onClick={() => props.setHexBlockColor("#FF661A")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FF6680" }}
-                    onClick={() => props.setHexBlockColor("#FF6680")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#0FBD8C" }}
-                    onClick={() => props.setHexBlockColor("#0FBD8C")}
-                />
-            </div>
-            <div>
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FF8080" }}
-                    onClick={() => props.setHexBlockColor("#FF8080")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FFB980" }}
-                    onClick={() => props.setHexBlockColor("#FFB980")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FFF480" }}
-                    onClick={() => props.setHexBlockColor("#FFF480")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#8EFF80" }}
-                    onClick={() => props.setHexBlockColor("#8EFF80")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#80FFBD" }}
-                    onClick={() => props.setHexBlockColor("#80FFBD")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#80EAFF" }}
-                    onClick={() => props.setHexBlockColor("#80EAFF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#80C1FF" }}
-                    onClick={() => props.setHexBlockColor("#80C1FF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#8084FF" }}
-                    onClick={() => props.setHexBlockColor("#8084FF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#D375FF" }}
-                    onClick={() => props.setHexBlockColor("#D375FF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#FF8AFF" }}
-                    onClick={() => props.setHexBlockColor("#FF8AFF")}
-                />
-                <button
-                    className={styles.presetColor}
-                    style={{ background: "#BBBBBB" }}
-                    onClick={() => props.setHexBlockColor("#BBBBBB")}
-                />
-            </div>
+    <div>
+        <div>
+            <b className={styles.colorText}>{'Color: '}</b>
+            <input
+                className={styles.colorInput}
+                value={props.blockColor}
+                type="color"
+                onChange={props.onBlockColorChange}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#4C97FF'}}
+                onClick={() => props.setHexBlockColor('#4C97FF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#9966FF'}}
+                onClick={() => props.setHexBlockColor('#9966FF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#CF63CF'}}
+                onClick={() => props.setHexBlockColor('#CF63CF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FFBF00'}}
+                onClick={() => props.setHexBlockColor('#FFBF00')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FFAB19'}}
+                onClick={() => props.setHexBlockColor('#FFAB19')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#5CB1D6'}}
+                onClick={() => props.setHexBlockColor('#5CB1D6')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#59C059'}}
+                onClick={() => props.setHexBlockColor('#59C059')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FF8C1A'}}
+                onClick={() => props.setHexBlockColor('#FF8C1A')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FF661A'}}
+                onClick={() => props.setHexBlockColor('#FF661A')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FF6680'}}
+                onClick={() => props.setHexBlockColor('#FF6680')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#0FBD8C'}}
+                onClick={() => props.setHexBlockColor('#0FBD8C')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FF8080'}}
+                onClick={() => props.setHexBlockColor('#FF8080')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FFB980'}}
+                onClick={() => props.setHexBlockColor('#FFB980')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FFF480'}}
+                onClick={() => props.setHexBlockColor('#FFF480')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#8EFF80'}}
+                onClick={() => props.setHexBlockColor('#8EFF80')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#80FFBD'}}
+                onClick={() => props.setHexBlockColor('#80FFBD')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#80EAFF'}}
+                onClick={() => props.setHexBlockColor('#80EAFF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#80C1FF'}}
+                onClick={() => props.setHexBlockColor('#80C1FF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#8084FF'}}
+                onClick={() => props.setHexBlockColor('#8084FF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#D375FF'}}
+                onClick={() => props.setHexBlockColor('#D375FF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#FF8AFF'}}
+                onClick={() => props.setHexBlockColor('#FF8AFF')}
+            />
+            <button
+                className={styles.presetColor}
+                style={{background: '#BBBBBB'}}
+                onClick={() => props.setHexBlockColor('#BBBBBB')}
+            />
         </div>
-    </details>
-)
+    </div>
+);
 
 const CustomProcedures = props => (
     <Modal
@@ -250,17 +238,17 @@ const CustomProcedures = props => (
                     />
                 </label>
                 <br />
-                {!props.editing ? (<div>
-                    <label>
+                {props.editing ? null : (<div className={styles.checkboxFlexRow}>
+                    <label className={styles.checkboxFlexStatic}>
+                        <b>{'Type: '}</b>
                         <input
                             checked={props.returns}
                             type="checkbox"
                             onChange={props.onToggleReturns}
                         />
-                        Returns a value
+                        {'Is a reporter'}
                     </label>
-                    <br />
-                    {props.returns ? (<div>
+                    {props.returns ? (<div className={styles.checkboxFlexRest}>
                         <label>
                             <input
                                 checked={props.selectedType === 'string'}
@@ -269,7 +257,7 @@ const CustomProcedures = props => (
                                 value="string"
                                 onChange={props.onOutputTypeChanged}
                             />
-                            Returns text
+                            {'Returns text'}
                         </label>
                         <label>
                             <input
@@ -279,7 +267,7 @@ const CustomProcedures = props => (
                                 value="number"
                                 onChange={props.onOutputTypeChanged}
                             />
-                            Returns number
+                            {'Returns number'}
                         </label>
                         <label>
                             <input
@@ -289,9 +277,9 @@ const CustomProcedures = props => (
                                 value="boolean"
                                 onChange={props.onOutputTypeChanged}
                             />
-                            Returns boolean
+                            {'Returns boolean'}
                         </label>
-                    </div>) : (<div>
+                    </div>) : (<div className={styles.checkboxFlexRest}>
                         <label>
                             <input
                                 checked={props.selectedType === 'statement'}
@@ -300,7 +288,7 @@ const CustomProcedures = props => (
                                 value="statement"
                                 onChange={props.onOutputTypeChanged}
                             />
-                            Normal
+                            {'Stack'}
                         </label>
                         <label>
                             <input
@@ -310,23 +298,18 @@ const CustomProcedures = props => (
                                 value="end"
                                 onChange={props.onOutputTypeChanged}
                             />
-                            End-cap
+                            {'Cap'}
                         </label>
                     </div>)}
-                </div>) : null}
-                <details>
-                    <summary>{dropdownCaret} Coloring and Styling</summary>
-                    <div style={{ marginLeft: '10px' }}>
-                        <BlockColorSection {...props} />
-                    </div>
-                    {/* <div style={{ marginLeft: '10px' }}>
-                        <details>
-                            <summary>{dropdownCaret} Debug</summary>
-                            <button onClick={() => props.onTestStart("icon")}>set block icon</button>
-                            <button onClick={() => props.onTestStart("removeicon")}>remove block icon</button>
-                        </details>
-                    </div> */}
-                </details>
+                </div>)}
+                <BlockColorSection {...props} />
+                {/* <div style={{ marginLeft: '10px' }}>
+                    <details>
+                        <summary>{dropdownCaret} Debug</summary>
+                        <button onClick={() => props.onTestStart("icon")}>set block icon</button>
+                        <button onClick={() => props.onTestStart("removeicon")}>remove block icon</button>
+                    </details>
+                 </div> */}
             </div>
             <Box className={styles.buttonRow}>
                 <button
@@ -368,7 +351,9 @@ CustomProcedures.propTypes = {
     returns: PropTypes.bool.isRequired,
     editing: PropTypes.bool.isRequired,
     selectedType: PropTypes.string.isRequired,
-    onOutputTypeChanged: PropTypes.func.isRequired
+    onOutputTypeChanged: PropTypes.func.isRequired,
+    setHexBlockColor: PropTypes.func.isRequired
 };
+BlockColorSection.propTypes = CustomProcedures.propTypes;
 
 export default injectIntl(CustomProcedures);
